ec841e7551b7ddbcf220ca222a31880300c4d9e0
mrroom <mrroom@gmail.com>
2025-03-14 16:37:53 +0900

    모델 변경경

--- ollama_app.py
+++ ollama_app.py
@@ -12,7 +12,7 @@ class GemmaChatBot:
         try:
             # Ollama 모델 설정
             self.chat_model = ChatOllama(
-                model="gemma3:4b",
+                model="phi4-mini:latest", #"gemma3:4b",
                 base_url="http://localhost:11434",
                 callbacks=[StreamingStdOutCallbackHandler()],
                 temperature=0.7,
